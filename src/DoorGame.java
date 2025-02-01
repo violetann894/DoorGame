173db1d99eca1212676e756d3f4e52829d9fe205
violetann894 <rachelhuss2976@gmail.com>
2025-01-31 21:23:27 -0500

    Started to build the runGame() method

--- src/DoorGame.java
+++ src/DoorGame.java
@@ -3,27 +3,16 @@ import java.util.Random;
 
 public class DoorGame {
 
-    //Need to randomize list for each experiment
-    //make sure to code in that it only shows a bad prize when revealing a door
+    private int switchWins;
 
-    private ArrayList<Double> percentages;
-
-    private int countOfWinsSwitch;
-
-    private int countOfWinsStay;
-
-    private ArrayList<Door> doors;
-
-    public DoorGame(){
-        doors = generateDoors();
-    }
+    private int stayWins;
 
     private ArrayList<Door> generateDoors(){
         ArrayList<Door> shuffledDoors = new ArrayList<>();
 
         ArrayList<Door> orderedDoors = new ArrayList<>();
-        Door door1 = new Door("Goat 1");
-        Door door2 = new Door("Goat 2");
+        Door door1 = new Door("Goat");
+        Door door2 = new Door("Goat");
         Door door3 = new Door("Car");
 
         orderedDoors.add(door1);
@@ -40,16 +29,49 @@ public class DoorGame {
 
         }
 
-        return doors;
+        return shuffledDoors;
     }
 
-    public ArrayList<Double> runGame(){
+    public ArrayList<Double> runGame(int trials){
 
-        return ;
+        //Stay trials
+        for(int i = 0; i < trials; i++){
+            ArrayList<Door> doors = generateDoors();
+
+            Random random = new Random();
+
+            int choice = random.nextInt(0, doors.size());
+
+
+
+            if(doors.get(choice).getBehindDoor().equals("Car")){
+                stayWins++;
+            }
+        }
+
+        //Switch trials
+        for(int i = 0; i < trials; i++){
+            ArrayList<Door> doors = generateDoors();
+
+            Random random = new Random();
+
+            int choice = random.nextInt(0, doors.size());
+
+
+
+            if(doors.get(choice).getBehindDoor().equals("Car")){
+                switchWins++;
+            }
+        }
+
+        return calculatePercentages(stayWins, switchWins, trials);
     }
 
-    private ArrayList<Double> calculatePercentages(int stayWins, int switchWins){
+    private ArrayList<Double> calculatePercentages(int stayWins, int switchWins, int numberOfTrials){
+        ArrayList<Double> percentages = new ArrayList<>();
 
+        percentages.add((double)stayWins/ (double)numberOfTrials);
+        percentages.add((double)switchWins/ (double)numberOfTrials);
 
         return percentages;
     }
