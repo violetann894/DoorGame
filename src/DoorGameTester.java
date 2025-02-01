80941c455459bfc491dc61194e79c5090db0170a
violetann894 <rachelhuss2976@gmail.com>
2025-01-31 22:28:51 -0500

    Added comments

--- src/DoorGameTester.java
+++ src/DoorGameTester.java
@@ -1,13 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * The DoorGameTester class is used to run the door game and figure out the percentages associated with switching door
+ * choice or keeping it the same.
+ * @author Rachel Hussmann
+ */
 public class DoorGameTester {
 
     public static void main(String[] args) {
 
+        //Initializes the DoorGame tester object
         DoorGame tester = new DoorGame();
+
+        //Initializes the results ArrayList to hold the calculated percentages of the trials
         ArrayList<Double> results = tester.runGame(10000);
-        System.out.println("Percentage of wins when choice was the same: " + results.get(0) + "%");
-        System.out.println("Percentage of wins when choice switched: " + results.get(1) + "%");
+
+        //Print statements to print out the results of the trials
+        System.out.printf("Percentage of wins when choice was the same: %.2f \n", results.get(0)*100);
+        System.out.printf("Percentage of wins when choice switched: %.2f \n", results.get(1)*100);
 
     }
 
