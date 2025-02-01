0ab885840d8debadc095132b75c9beda24bcd55e
violetann894 <rachelhuss2976@gmail.com>
2025-01-31 22:21:47 -0500

    Added comments and removed unnecessary variable

--- src/Door.java
+++ src/Door.java
@@ -1,30 +1,33 @@
-
+/**
+ * The Door class is responsible for holding information about a door being used in a DoorGame.
+ * @author Rachel Hussmann
+ */
 public class Door {
 
+    //Initializes the string that states what is behind the door
     private String behindDoor;
 
-    private String nameOfDoor;
-
-    public Door(String behindDoor, String nameOfDoor){
+    /**
+     * Constructor for the Door class.
+     * @param behindDoor This constructor accepts a string variable that states what is behind the new door object
+     */
+    public Door(String behindDoor){
         this.behindDoor = behindDoor;
-
-        this.nameOfDoor = nameOfDoor;
     }
 
+    /**
+     * The getBehindDoor method returns the string that states what is behind the door
+     * @return This method returns the string of what is behind the door
+     */
     public String getBehindDoor(){
         return behindDoor;
     }
 
+    /**
+     * The setBehindDoor method accepts a new string that changes what is behind the door.
+     * @param behindDoor This method accepts a String with the new object behind the door.
+     */
     public void setBehindDoor(String behindDoor){
         this.behindDoor = behindDoor;
     }
-
-    public String getNameOfDoor() {
-        return nameOfDoor;
-    }
-
-    public void setNameOfDoor(String nameOfDoor) {
-        this.nameOfDoor = nameOfDoor;
-    }
-
 }
