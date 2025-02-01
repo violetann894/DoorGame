8468deca223fdd6113a2e8b4d568b0eee7d87261
violetann894 <rachelhuss2976@gmail.com>
2025-01-31 21:17:32 -0500

    Added tester statements

--- src/DoorGameTester.java
+++ src/DoorGameTester.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class DoorGameTester {
 
     public static void main(String[] args) {
+
+        DoorGame tester = new DoorGame();
+        ArrayList<Double> results = tester.runGame(10000);
+        System.out.println("Percentage of wins when choice was the same: " + results.get(0) + "%");
+        System.out.println("Percentage of wins when choice switched: " + results.get(1) + "%");
 
     }
 
